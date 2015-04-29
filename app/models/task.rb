b8d0019b80676fa83697ebe3dca588479876daa2
vagrant <vagrant@localhost.localdomain>
2015-04-29 04:27:04 +0000

    Validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
+
+	validates :title, :presence => true
+	validates :duration, :numericality => {:only_integer => true}
+
 	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :username, :presence => true
+  validates :name, :presence => true
+  validates :lastname, :presence => true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :tasks
